3be24b68a516cb6949e6a88b31c40dcfe51d39b1
tomlynchcode <tl4@umbc.edu>
2016-11-30 12:42:54 -0500

    Add files via upload

---
+++ GPA.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+
+public class GPA {
+
+    public static void main(String[] args) {
+        double GPA1;
+        System.out.println("Please enter your cumulative GPA: ");
+        double GPA2;
+        System.out.println("Please enter your semester GPA: ");
+        System.out.println("Your current GPA is calculated as: ");
+
+
+    }
+
+    public static double avg(){
+
+
+    }
+}
